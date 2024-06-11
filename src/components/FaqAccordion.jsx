b6ab226e9fe65c5ad11b335f73ac58ac39c0884b
guzmanpdro <pedroguzman2598@gmail.com>
2024-06-10 23:26:01 -0400

    Add styles to the FaqAccordion component

--- src/components/FaqAccordion.jsx
+++ src/components/FaqAccordion.jsx
@@ -1,17 +1,18 @@
 import { IconStar } from "./Icons"
 import FaqList from "./FaqList"
+import './FaqAccordion.css'
 
 function FaqAccordion({ title, faqs }) {
   return (
-    <article>
-      <h1>
+    <article className="accordion">
+      <h1 className="title">
         <span>
           <IconStar />
         </span>
         {title}
       </h1>
       <FaqList faqs={faqs} />
-    </article>
+    </article >
   )
 }
 
